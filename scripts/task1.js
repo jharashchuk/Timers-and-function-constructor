43ea356e3f63dd3a26ef97051e93a777b6cc190f
jharashchuk <j.garaschuk@gmail.com>
2021-12-01 20:13:19 +0100

    add new timer

--- scripts/task1.js
+++ scripts/task1.js
@@ -4,19 +4,38 @@
 // - Расширим функцию таким образом, чтобы на цифре 60 таймер останавливался.
 // - При остановке таймера выведем в консоль запись о том, что минута прошла.
 
-// 
-function showNumber(number) {
-    console.log(number);
-    let counter = 0;
+
+// function showNumber(number) {
+//     console.log(number);
+//     let counter = 0;
     
-    let timerId = setInterval(() => {
-        number += 1;
-        console.log(number);
-        counter++;
-        if(counter == 60) {
+//     const timerId = setInterval(() => {
+//         number += 1;
+//         console.log(number);
+//         counter++;
+//         if(counter == 60) {
+//             console.log('minute passed');
+//             clearInterval(timerId);                   
+//         }
+//     }, 1000);   
+// }
+
+// showNumber(10);
+
+function timer(num) {    
+    let counter = 0;
+
+    const timerId = setTimeout(function go() {
+        console.log(num);
+            num += 1;
+        if(counter < 60) {
+            setTimeout(go, 1000);
+        } else {
             console.log('minute passed');
-            clearInterval(timerId);                   
+            clearTimeout(timerId);       
         }
-    }, 1000);   
+        counter++;
+    }, 1000);    
 }
-showNumber(10);
+
+timer(20);
